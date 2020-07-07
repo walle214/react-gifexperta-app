885efa54d6b6cab063ea44e362af210ab6453162
Ulises Ramirez <vwalle.r.m@gmail.com>
2020-07-07 13:16:41 -0500

    First commit

---
+++ docs/precache-manifest.81812ce19bd86d31b248cab2945b467f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4363a081d48db90bc5e28d81a3b84f07",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a08fd9c6afdf212cbfae",
+    "url": "/static/css/main.6a898f0f.chunk.css"
+  },
+  {
+    "revision": "3ec50c8fead662706b17",
+    "url": "/static/js/2.637c77a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.637c77a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a08fd9c6afdf212cbfae",
+    "url": "/static/js/main.8af5aba1.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
